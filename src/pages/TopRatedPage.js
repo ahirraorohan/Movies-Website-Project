a053a49636cc62b902b51a99495ff24f51e37c66
ahirraorohan <150143780+ahirraorohan@users.noreply.github.com>
2024-08-30 09:35:21 +0530

    Create TopRatedPage.js

---
+++ src/pages/TopRatedPage.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import MovieCard from '../components/MovieCard';
+
+const TopRatedPage = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=c45a857c193f6302f2b5061c3b85e743`)
+            .then(response => response.json())
+            .then(data => setMovies(data.results));
+    }, []);
+
+    return (
+        <div className="movie-list">
+            {movies.map(movie => (
+                <MovieCard key={movie.id} movie={movie} />
+            ))}
+        </div>
+    );
+};
+
+export default TopRatedPage;
