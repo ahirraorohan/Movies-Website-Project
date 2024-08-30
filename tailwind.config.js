3d430b3881ca1fa0f9fd198a2a30c2eefd521eb8
ahirraorohan <150143780+ahirraorohan@users.noreply.github.com>
2024-08-30 10:23:03 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      body: ["Bricolage Grotesque", "sans-serif"],
+      title: ["Lobster Two", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
